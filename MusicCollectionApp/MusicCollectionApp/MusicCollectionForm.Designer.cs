f7aefdaf289c77317088827c8394f0d336b2f950
Stakhaevich <verast2006@gmail.com>
2026-03-30 11:58:41 +0300

    Изменение размера кнопок на форме MusicCollectionForm

--- MusicCollectionApp/MusicCollectionApp/MusicCollectionForm.Designer.cs
+++ MusicCollectionApp/MusicCollectionApp/MusicCollectionForm.Designer.cs
@@ -23,48 +23,70 @@
             this.searchByArtistButton = new System.Windows.Forms.Button();
             this.sortByYearButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
+            // 
             // listView
-            this.listView.Location = new System.Drawing.Point(10, 10);
-            this.listView.Size = new System.Drawing.Size(480, 300);
-            this.listView.View = System.Windows.Forms.View.Details;
+            // 
             this.listView.FullRowSelect = true;
-            this.listView.Columns.Add("Исполнитель", 150);
-            this.listView.Columns.Add("Название", 150);
-            this.listView.Columns.Add("Жанр", 100);
-            this.listView.Columns.Add("Год", 50);
-
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(13, 12);
+            this.listView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(639, 368);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
             // addTrackButton
-            this.addTrackButton.Location = new System.Drawing.Point(10, 320);
-            this.addTrackButton.Size = new System.Drawing.Size(100, 25);
+            // 
+            this.addTrackButton.Location = new System.Drawing.Point(13, 394);
+            this.addTrackButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.addTrackButton.Name = "addTrackButton";
+            this.addTrackButton.Size = new System.Drawing.Size(133, 45);
+            this.addTrackButton.TabIndex = 1;
             this.addTrackButton.Text = "Добавить трек";
-
+            // 
             // removeTrackButton
-            this.removeTrackButton.Location = new System.Drawing.Point(120, 320);
-            this.removeTrackButton.Size = new System.Drawing.Size(100, 25);
+            // 
+            this.removeTrackButton.Location = new System.Drawing.Point(160, 394);
+            this.removeTrackButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.removeTrackButton.Name = "removeTrackButton";
+            this.removeTrackButton.Size = new System.Drawing.Size(133, 45);
+            this.removeTrackButton.TabIndex = 2;
             this.removeTrackButton.Text = "Удалить трек";
-
+            // 
             // searchByArtistButton
-            this.searchByArtistButton.Location = new System.Drawing.Point(230, 320);
-            this.searchByArtistButton.Size = new System.Drawing.Size(120, 25);
+            // 
+            this.searchByArtistButton.Location = new System.Drawing.Point(307, 394);
+            this.searchByArtistButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.searchByArtistButton.Name = "searchByArtistButton";
+            this.searchByArtistButton.Size = new System.Drawing.Size(160, 45);
+            this.searchByArtistButton.TabIndex = 3;
             this.searchByArtistButton.Text = "Поиск по исполнителю";
-
+            // 
             // sortByYearButton
-            this.sortByYearButton.Location = new System.Drawing.Point(360, 320);
-            this.sortByYearButton.Size = new System.Drawing.Size(120, 25);
+            // 
+            this.sortByYearButton.Location = new System.Drawing.Point(480, 394);
+            this.sortByYearButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.sortByYearButton.Name = "sortByYearButton";
+            this.sortByYearButton.Size = new System.Drawing.Size(160, 45);
+            this.sortByYearButton.TabIndex = 4;
             this.sortByYearButton.Text = "Сортировать по году";
-
+            // 
             // MusicCollectionForm
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500, 400);
+            this.ClientSize = new System.Drawing.Size(667, 492);
             this.Controls.Add(this.listView);
             this.Controls.Add(this.addTrackButton);
             this.Controls.Add(this.removeTrackButton);
             this.Controls.Add(this.searchByArtistButton);
             this.Controls.Add(this.sortByYearButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MusicCollectionForm";
             this.Text = "Управление музыкальной коллекцией";
             this.ResumeLayout(false);
+
         }
 
         #endregion
